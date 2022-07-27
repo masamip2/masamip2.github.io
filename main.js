6a55c7223ab9d604d6f4ffdb6e26837eac754ab3
masamip2 <masamip2@illinois.edu>
2022-07-27 11:17:07 -0700

    test

--- main.js
+++ main.js
@@ -590,9 +590,8 @@ let bubbleBk = null;
 async function plot(data) {
 
     if (data) {
-        bubbleBk.transition().duration(500).style("opacity", 0.6);
+        bubbleBk.transition().duration(500).style("opacity", 0.6).transition().duration(500).style("opacity", 0);
         removeBubbles();
-        bubbleBk.transition().duration(500).style("opacity", 0);
     } else {
         removeBubbles();
         data = dataset;
